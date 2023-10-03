97084465e5b5cf0774c79dfdf1cccc67616e941f
Singh2311 <ss9532729@gmail.com>
2023-10-03 10:35:13 +0530

    add button

--- app/components/ProductDetails/index.tsx
+++ app/components/ProductDetails/index.tsx
@@ -1,3 +1,4 @@
+import Counter from "../Counter";
 import { ReadMore } from "../ReadMore";
 
 export default function ProductDetails() {
@@ -17,7 +18,7 @@ export default function ProductDetails() {
       <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 dark:text-white mt-2">6061 Aluminum Plate 0.25"</h1>
       <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 dark:text-white mt-2">$153<sup>83</sup></h1>
     </div>
-    <div className="py-4 border-b border-gray-200 flex items-center justify-between">
+    <div className="py-4 flex items-center justify-between">
       {/* <p className="text-base leading-4 text-gray-800 dark:text-gray-300">Colours</p> */}
       <ReadMore>
       GeeksforGeeks: A Computer Science portal for geeks. 
@@ -30,7 +31,7 @@ export default function ProductDetails() {
           Opportunities. Knowledge is power!
       </ReadMore>
     </div>
-    <div className="py-4 border-b border-gray-200 flex items-center justify-between">
+    {/* <div className="py-4 border-b border-gray-200 flex items-center justify-between">
       <p className="text-base leading-4 text-gray-800 dark:text-gray-300">Size</p>
       <div className="flex items-center justify-center">
         <p className="text-sm leading-none text-gray-600 dark:text-gray-300 mr-3">38.2</p>
@@ -38,16 +39,85 @@ export default function ProductDetails() {
           <path d="M1 1L5 5L1 9" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
       </div>
+    </div> */}
+    <div className="flex items-center w-10/12">
+    <label for="countries" className="block mb-2 mr-3 text-sm font-medium text-gray-900 dark:text-white flex items-center">Thickness(A)</label>
+<select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <option selected>Choose a Thickness</option>
+  <option value="US">0.25</option>
+  <option value="CA">0.50</option>
+  <option value="FR">0.75</option>
+  <option value="DE">0.85</option>
+</select>
     </div>
-    <button className="dark:bg-white dark:text-gray-900 dark:hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
-      <svg className="mr-3 text-white dark:text-gray-900" width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.02301 7.18999C7.48929 6.72386 7.80685 6.12992 7.93555 5.48329C8.06425 4.83666 7.9983 4.16638 7.74604 3.55724C7.49377 2.94809 7.06653 2.42744 6.51835 2.06112C5.97016 1.6948 5.32566 1.49928 4.66634 1.49928C4.00703 1.49928 3.36252 1.6948 2.81434 2.06112C2.26615 2.42744 1.83891 2.94809 1.58665 3.55724C1.33439 4.16638 1.26843 4.83666 1.39713 5.48329C1.52583 6.12992 1.8434 6.72386 2.30968 7.18999L4.66634 9.54749L7.02301 7.18999Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M4.66699 4.83333V4.84166" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M13.69 13.8567C14.1563 13.3905 14.4738 12.7966 14.6025 12.15C14.7312 11.5033 14.6653 10.8331 14.413 10.2239C14.1608 9.61476 13.7335 9.09411 13.1853 8.72779C12.6372 8.36148 11.9926 8.16595 11.3333 8.16595C10.674 8.16595 10.0295 8.36148 9.48133 8.72779C8.93314 9.09411 8.5059 9.61476 8.25364 10.2239C8.00138 10.8331 7.93543 11.5033 8.06412 12.15C8.19282 12.7966 8.51039 13.3905 8.97667 13.8567L11.3333 16.2142L13.69 13.8567Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M11.333 11.5V11.5083" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-      </svg>
-      Check availability in store
-    </button>
+    <div className="flex items-center space-x-4 mt-4">
+    <label className="block mb-2 mr-6 text-sm font-medium text-gray-900 dark:text-white flex items-center mt-2">Width(B)</label>
+
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="option1" name="radio-group" className="hidden" />
+      <label for="option1" class="cursor-pointer bg-white border border-gray-300 p-2">
+        48.5
+      </label>
+    </div>
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="option2" name="radio-group" className="hidden" />
+      <label for="option2" class="cursor-pointer bg-white border border-gray-300 p-2">
+        68.5
+      </label>
+    </div>
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="option3" name="radio-group" className="hidden" />
+      <label for="option3" class="cursor-pointer bg-white border border-gray-300 p-2">
+        Custom
+      </label>
+    </div>
+  </div>
+  <div className="flex items-center space-x-4 mt-4">
+    <label className="block mb-2 mr-4 text-sm font-medium text-gray-900 dark:text-white flex items-center mt-2">Length(C)</label>
+
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="length1" name="radio-group1" className="hidden" />
+      <label for="length1" class="cursor-pointer bg-white border border-gray-300 p-2">
+        144.5
+      </label>
+    </div>
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="length2" name="radio-group1" className="hidden" />
+      <label for="length2" class="cursor-pointer bg-white border border-gray-300 p-2">
+      Custom
+      </label>
+    </div>
+  </div>
+  <div className="flex items-center space-x-4 mt-4">
+    <label className="block mb-2 mr-2-3 text-sm font-medium text-gray-900 dark:text-white flex items-center mt-2">Source</label>
+
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="source1" name="radio-group1" className="hidden" />
+      <label for="source1" class="cursor-pointer bg-white border border-gray-300 p-2">
+        Domestic
+      </label>
+    </div>
+    <div className="relative inline-flex items-center">
+      <input type="radio" id="source2" name="radio-group1" className="hidden" />
+      <label for="source2" class="cursor-pointer bg-white border border-gray-300 p-2">
+      Import
+      </label>
+    </div>
+  </div>
+  <div className="flex items-center space-x-4 mt-4">
+    <p className="font-bold mt-4">$303</p>
+  <Counter />
+
+  </div>
+  <div className="mt-4">
+  <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-72">Add to Cart</button>
+
+  </div>
+  <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+    <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+    <label for="bordered-radio-1" className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Schedule a recurring Order</label>
+</div>
+
     <div>
       <p className="xl:pr-48 text-base lg:leading-tight leading-normal text-gray-600 dark:text-gray-300 mt-7">It is a long established fact that a reader will be distracted by thereadable content of a page when looking at its layout. The point of usingLorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
       <p className="text-base leading-4 mt-7 text-gray-600 dark:text-gray-300">Product Code: 8BN321AF2IF0NYA</p>
