b155df87c1d603497cb64189416cfe0d3aa19abe
Singh2311 <ss9532729@gmail.com>
2023-10-03 21:43:09 +0530

    add breadcurm

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,7 +4,6 @@ import ProductDetails from '../../../components/ProductDetails';
 import React from 'react';
 
 export default function ProductId({ params }:any) {
-  console.log(params,'params')
   return (
     params && <div className="mx-auto">
         <ProductDetails id={params.id} />
